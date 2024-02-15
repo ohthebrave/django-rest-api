ed246c73c550cc44fc0f4475a11a41f5b5d0d2ca
George <georgejoj28@gmail.com>
2024-02-15 14:30:36 +0300

    add pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,6 +90,10 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
