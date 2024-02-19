b773e8c08d38081c1acba9c44d093d05a6b690c2
George <georgejoj28@gmail.com>
2024-02-19 17:45:20 +0300

    add category model

--- online/admin.py
+++ online/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Animal
+from .models import User, Animal, Category
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Animal)
+admin.site.register(Category)
